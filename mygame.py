59ba8523ecc7a4bf43cacfbabb9fc9f99cabc833
Marko Ilibasic <ilibasicmarko@gmail.com>
2020-09-10 13:20:04 +0200

    First commit

---
+++ mygame.py
@@ -0,0 +1,25 @@
+# This is first guessning game
+
+import random
+
+#Defining variables
+
+n= random.randint(1,99)
+guess= int(input("Enter an integer from 1 to 99: "))
+
+#Do a loop
+
+while n != "guess":
+    print
+    if guess <n:
+        print ("guess is low")
+        guess = int(input("Enter an integer from 1 to 99: "))
+    elif guess > n:
+        print ("guess is high")
+        guess = int(input("Enter an integer from 1 to 99: "))
+    else:
+        print ("you guessed it! ")
+        break
+    print
+        
+
